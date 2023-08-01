5572a4ca99513cce59bfd6d040354eddb183fa70
ort <orthodoxplus@ya.ru>
2023-08-01 17:00:52 +0300

    fix reading files and output

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Slf4j
@@ -41,62 +43,83 @@ public class Main {
 						.filter(o -> o.startDate().isBefore(date))
 						.filter(o -> o.endDate().isAfter(date))
 						.filter(o -> objectIds.contains(o.id()))
-						.forEach(o -> System.out.printf("%d %s", o.id(), o.name()));
+						.forEach(o -> System.out.printf("%d %s%n", o.id(), o.name()));
 			}
 			case 2 -> log.info("Task 2 not implemented");
 		}
 	}
 
 	private static Tuple2<List<AddrObject>, List<AdmHierarchy>> readFiles(Args args) {
-		Path objectsFile;
-		Path hierarchyFile;
+		Optional<Tuple2<Path, Path>> files = searchFilesInArgs(args);
+		if (files.isEmpty()){
+			files = searchFilesInCurrentDir();
 
-		if (args.getObjectsFile().isEmpty() || args.getHierarchyFile().isEmpty()) {
-			Path currentDir = Path.of(System.getProperty("user.dir"));
-
-			try (Stream<Path> walk = Files.walk(currentDir)) {
-				long countFiles = walk
-						.filter(file -> {
-							String fileName = file.getFileName().toString();
-							return fileName.equals("AS_ADDR_OBJ.XML") || fileName.equals("AS_ADM_HIERARCHY.XML");
-						})
-						.count();
-				if (countFiles != 2) {
-					log.error("Error looking files in current directory");
-					throw new RuntimeException();
-				} else {
-					objectsFile = Path.of("AS_ADDR_OBJ.XML");
-					hierarchyFile = Path.of("AS_ADM_HIERARCHY.XML");
-				}
-			} catch (IOException e) {
-				log.error("Error looking files in current directory", e);
-				throw new RuntimeException(e);
+			if (files.isEmpty()) {
+				log.error("Files not found");
+				System.exit(1);
 			}
-		} else {
-			objectsFile = Path.of(args.getObjectsFile().get());
-			hierarchyFile = Path.of(args.getHierarchyFile().get());
 		}
 
-		return Tuple.tuple(
-				readObjects(objectsFile),
-				readHierarchy(hierarchyFile)
-		);
+		return Tuple.tuple(readObjects(files.get().v1), readHierarchy(files.get().v2));
+	}
+
+	private static Optional<Tuple2<Path, Path>> searchFilesInArgs(Args args) {
+		if (args.getObjectsFile().isPresent() && args.getHierarchyFile().isPresent()) {
+			return Optional.of(Tuple.tuple(
+					Path.of(args.getObjectsFile().get()),
+					Path.of(args.getHierarchyFile().get())
+			));
+		}
+		return Optional.empty();
+	}
+
+	private static Optional<Tuple2<Path, Path>> searchFilesInCurrentDir() {
+		Path currentDir = Path.of(System.getProperty("user.dir"));
+		try (Stream<Path> walk = Files.walk(currentDir)) {
+			Set<Path> xmlFiles = walk
+					.filter(file -> file.getFileName().toString().toLowerCase().endsWith(".xml"))
+					.collect(Collectors.toSet());
+			Path objectsFile = null;
+			Path hierarchyFile = null;
+			for (Path file : xmlFiles) {
+				String fileName = file.getFileName().toString();
+				if (fileName.equals("AS_ADDR_OBJ.XML")) objectsFile = file;
+				if (fileName.equals("AS_ADM_HIERARCHY.XML")) hierarchyFile = file;
+			}
+			if (objectsFile == null || hierarchyFile == null) {
+				log.error("Files not found in current directory");
+				System.exit(1);
+			}
+
+			return Optional.of(Tuple.tuple(objectsFile, hierarchyFile));
+		} catch (IOException e) {
+			log.error("Error looking files in current directory", e);
+			return Optional.empty();
+		}
 	}
 
 	private static List<AddrObject> readObjects(Path file) {
 		try {
+			if (Files.notExists(file)) {
+				log.error("File {} not found", file);
+				System.exit(1);
+			}
 			return xml.readValue(Files.readString(file), AddrObjectList.class).getObjects();
 		} catch (IOException e) {
-			log.error("Error reading objects file {}", file);
+			log.error("Error reading objects file {}", file, e);
 			return List.of();
 		}
 	}
 
 	private static List<AdmHierarchy> readHierarchy(Path file) {
 		try {
+			if (Files.notExists(file)) {
+				log.error("File {} not found", file);
+				System.exit(1);
+			}
 			return xml.readValue(Files.readString(file), AdmHierarchyList.class).getElements();
 		} catch (IOException e) {
-			log.error("Error reading hierarchy file {}", file);
+			log.error("Error reading hierarchy file {}", file, e);
 			return List.of();
 		}
 	}
